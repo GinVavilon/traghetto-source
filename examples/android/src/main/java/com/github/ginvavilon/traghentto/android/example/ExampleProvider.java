92d25b54c020b0593aa96de333815b0d4cbcf40e
GinVavilon <ginVavilon@gmail.com>
2019-02-04 00:04:08 +0300

    Add example pf android document provider

---
+++ examples/android/src/main/java/com/github/ginvavilon/traghentto/android/example/ExampleProvider.java
@@ -0,0 +1,225 @@
+/**
+ *
+ */
+package com.github.ginvavilon.traghentto.android.example;
+
+import android.content.res.AssetManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.github.ginvavilon.traghentto.BaseSource;
+import com.github.ginvavilon.traghentto.Logger;
+import com.github.ginvavilon.traghentto.Source;
+import com.github.ginvavilon.traghentto.StreamResource;
+import com.github.ginvavilon.traghentto.android.AndroidLogHadler;
+import com.github.ginvavilon.traghentto.android.AssetSource;
+import com.github.ginvavilon.traghentto.android.ResourceSource;
+import com.github.ginvavilon.traghentto.android.provider.SimpleSourceProvider;
+import com.github.ginvavilon.traghentto.crypto.CryptoConfiguration;
+import com.github.ginvavilon.traghentto.crypto.EncryptoSource;
+import com.github.ginvavilon.traghentto.exceptions.IOSourceException;
+import com.github.ginvavilon.traghentto.file.FileSource;
+import com.github.ginvavilon.traghentto.params.StreamParams;
+
+
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+public class ExampleProvider extends SimpleSourceProvider {
+
+    private static final String ENCRYPED_ROOT = "encryped-files";
+    private static final String ENCRYPED_DIR = "encryped";
+    private static final String ASSET_ROOT = "assets";
+    private static final String FILES_ROOT = "files";
+    private static final String RESOURCES_ROOT = "resources";
+    public static final String PATH = "public";
+    public static final String IMAGE_PNG_MIME_TYPE = "image/png";
+
+    static {
+        AndroidLogHadler.init();
+    }
+
+    @Override
+    protected int getRootIcon(String root) {
+        switch (root) {
+            case ENCRYPED_ROOT:
+                return R.drawable.ic_encrypted;
+            default:
+                return R.mipmap.ic_launcher_round;
+        }
+    }
+
+    @Override
+    protected String getRootTitle(String root) {
+        switch (root) {
+        case ASSET_ROOT:
+            return getContext().getString(R.string.label_assets);
+        case FILES_ROOT:
+            return getContext().getString(R.string.label_files);
+        case RESOURCES_ROOT:
+            return getContext().getString(R.string.label_resources);
+        case ENCRYPED_ROOT:
+            return getContext().getString(R.string.label_encrypted);
+        default:
+            return getContext().getString(R.string.app_name);
+        }
+    }
+
+    @Override
+    protected String getRootSummary(String root) {
+        return null;
+    }
+
+    @Override
+    protected List<String> createRootNames() {
+        return Arrays.asList(ENCRYPED_ROOT, FILES_ROOT, ASSET_ROOT, RESOURCES_ROOT);
+    }
+
+    @Override
+    protected Source getRootSource(String name) {
+
+        switch (name) {
+        case FILES_ROOT:
+            return new FileSource(getContext().getFilesDir());
+        case ENCRYPED_ROOT:
+            return createdCryptedSource();
+        case RESOURCES_ROOT:
+            SimpleSource sources = new SimpleSource();
+            sources.add(new ResourceSource(getContext().getResources(), R.mipmap.ic_launcher));
+            sources.add(new ResourceSource(getContext().getResources(), R.mipmap.sample));
+            sources.add(new ResourceSource(getContext().getResources(), R.mipmap.ic_launcher_round));
+            sources.add(new ResourceSource(getContext().getResources(), R.mipmap.ic_launcher_foreground));
+            return sources;
+        case ASSET_ROOT:
+        default:
+            AssetManager assets = getContext().getAssets();
+
+            return new AssetSource(assets, PATH);
+
+        }
+    }
+
+    private Source createdCryptedSource() {
+        try {
+            File file = new File(getContext().getFilesDir(), ENCRYPED_DIR);
+            CryptoConfiguration configuration = CryptoConfiguration
+                    .builder()
+                    .usePassword("testKey8Exampl56")
+                    .addRandomSalt(5, 12)
+                    .build();
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+
+            return new EncryptoSource<FileSource>(new FileSource(file), configuration);
+
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            Logger.e(e);
+        }
+
+        return null;
+    }
+
+    @Override
+    protected int getIcon(Source source, String mimeType) {
+        return R.mipmap.ic_launcher;
+    }
+
+    private class SimpleSource extends BaseSource {
+
+        private Map<String, Source> mSources = new HashMap<>();
+
+        public void add(Source source) {
+            mSources.put(source.getPath(), source);
+        }
+
+        @Override
+        public List<? extends Source> getChildren() {
+            return new ArrayList<>(mSources.values());
+        }
+
+        @Override
+        public Source getChild(String name) {
+            Logger.d(Logger.Level.MODULE, "get child %s", name);
+            return mSources.get(name);
+        }
+
+        @Override
+        public boolean isConteiner() {
+            return true;
+        }
+
+        @Override
+        protected InputStream openInputStream(StreamParams pParams)
+                throws IOException, IOSourceException {
+            return null;
+        }
+
+        @Override
+        public StreamResource<InputStream> openResource(StreamParams pParams)
+                throws IOSourceException, IOException {
+            return null;
+        }
+
+        @Override
+        public String getPath() {
+            return "virtual";
+        }
+
+        @Override
+        public String getName() {
+            return "virtual";
+        }
+
+        @Override
+        public String getUriString() {
+            return "virtual://virtual";
+        }
+
+        @Override
+        public boolean exists() {
+            return true;
+        }
+
+        @Override
+        public long getLenght() {
+            return 0;
+        }
+
+        @Override
+        public boolean isLocal() {
+            return true;
+        }
+
+        @Override
+        public boolean isDataAvailable() {
+            return true;
+        }
+
+    }
+
+    @Override
+    protected String detectRoot(String id, Source source) {
+        if (source instanceof ResourceSource) {
+            return RESOURCES_ROOT;
+        }
+        return super.detectRoot(id, source);
+    }
+
+    @Override
+    protected String getTypeForSource(Source source) {
+        if (source instanceof ResourceSource) {
+            return IMAGE_PNG_MIME_TYPE;
+        }
+        return super.getTypeForSource(source);
+    }
+}
--- examples/android/src/main/java/com/github/ginvavilon/traghentto/android/example/MainActivity.java
+++ examples/android/src/main/java/com/github/ginvavilon/traghentto/android/example/MainActivity.java
@@ -1,16 +1,22 @@
 package com.github.ginvavilon.traghentto.android.example;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -20,6 +26,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.Executors;
 
+import com.github.ginvavilon.traghentto.DelegatedSource;
+import com.github.ginvavilon.traghentto.DeletableSource;
 import com.github.ginvavilon.traghentto.Logger;
 import com.github.ginvavilon.traghentto.Source;
 import com.github.ginvavilon.traghentto.SourceUtils;
@@ -36,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String DEFAULT_NAME = "new";
     public static final String OPEN_TYPE = "*/*";
+    public static final String IMAGE_MIME_TIPE_PREFIX = "image/";
 
     static {
         AndroidLogHadler.init();
@@ -45,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mOutputText;
 
     private ProgressBar mProgress;
+    private String mMimeType = "image/png";
+    private TextView mTypeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,15 +68,15 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(this::startCopy);
         mInputText = findViewById(R.id.txt_input);
-        mInputText.setText(SourceFactory.createFromResource(this, R.mipmap.ic_launcher).getUriString());
+        mTypeText = findViewById(R.id.type);
+        Source initialInput = SourceFactory.createFromResource(this, R.mipmap.sample);
+        mInputText.setText(initialInput.getUriString());
+        show(initialInput);
         mOutputText = findViewById(R.id.txt_output);
-        findViewById(R.id.btn_input).setOnClickListener(this::openInput);
-        findViewById(R.id.btn_output_open).setOnClickListener(this::openOutput);
-        findViewById(R.id.btn_output_create).setOnClickListener(this::createOutput);
     }
 
     private void startCopy(View view) {
-        Source input = SourceFactory.createFromUri(this, mInputText.getText().toString());
+        Source input = getInputSource();
         Source output = SourceFactory.createFromUri(this, mOutputText.getText().toString());
         mProgress.setMax((int) input.getLenght());
         new CopyAsyncTask(view).executeOnExecutor(Executors.newCachedThreadPool(), input, output);
@@ -77,17 +88,24 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(snackbar::show);
     }
 
-    private void createOutput(View view) {
-        createFile(DocumentSource.DEFAULT_MIME_TYPE, DEFAULT_NAME);
+    private void createOutput() {
+        createFile(mMimeType, DEFAULT_NAME);
     }
 
-    private void openOutput(View view) {
+    private void openOutput() {
         openIntent(OPEN_OUTPUT_REQUEST_CODE);
     }
 
-    private void openInput(View view) {
-
+    private void openInput() {
         openIntent(OPEN_INPUT_REQUEST_CODE);
+    }
+
+    private void openTreeInput() {
+        openTreeIntent(OPEN_INPUT_REQUEST_CODE);
+    }
+
+    private void openTreeOutput() {
+        openTreeIntent(OPEN_OUTPUT_REQUEST_CODE);
     }
 
     private static final int OPEN_INPUT_REQUEST_CODE = 42;
@@ -111,6 +129,57 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, code);
     }
 
+    private void openTreeIntent(int code) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+
+        startActivityForResult(intent, code);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_open:
+                openInput();
+                return true;
+            case R.id.item_open_tree:
+                openTreeInput();
+                return true;
+            case R.id.item_replace:
+                openOutput();
+                return true;
+            case R.id.item_create:
+                createOutput();
+                return true;
+            case R.id.item_save_tree:
+                openTreeOutput();
+                return true;
+            case R.id.item_delete:
+                onDelete();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onDelete() {
+        Source input = getInputSource();
+        if (input instanceof DeletableSource){
+            if (((DeletableSource) input).delete()){
+                Snackbar.make(mInputText, R.string.result_deleted,Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+            Snackbar.make(mInputText, R.string.fail_delete,Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
+        Snackbar.make(mInputText, R.string.failr_not_deletable,Snackbar.LENGTH_SHORT).show();
+
+    }
+
+    private Source getInputSource() {
+        return SourceFactory.createFromUri(this, mInputText.getText().toString());
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
@@ -131,22 +200,32 @@ public class MainActivity extends AppCompatActivity {
                 onOpenInput(uri);
                 break;
             case OPEN_OUTPUT_REQUEST_CODE:
+
                 onOpenOutput(uri);
                 break;
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void onOpenOutput(Uri uri) {
         DocumentSource source = new DocumentSource(getContentResolver(), uri);
         mOutputText.setText(source.getUriString());
     }
 
     private void onOpenInput(Uri uri) {
+
         DocumentSource inputSource = new DocumentSource(getContentResolver(), uri);
         mInputText.setText(inputSource.getUriString());
 
-        if (inputSource.getDocumentInfo().getMimeType().startsWith("image/")) {
+        mMimeType = inputSource.getDocumentInfo().getMimeType();
+        mTypeText.setText(mMimeType);
+        if (mMimeType.startsWith(IMAGE_MIME_TIPE_PREFIX)) {
             show(inputSource);
         }
 
@@ -199,7 +278,9 @@ public class MainActivity extends AppCompatActivity {
         protected Source doInBackground(Source... sources) {
 
             try {
-                SourceUtils.copy(sources[0], (WritableSource) sources[1], true, null, null, this);
+
+                WritableSource to = (WritableSource) sources[1];
+                SourceUtils.copy(sources[0], to, true, null, null, this);
 
             } catch (IOException | SourceAlreadyExistsException | IOSourceException e) {
                 showError(mView, e);
